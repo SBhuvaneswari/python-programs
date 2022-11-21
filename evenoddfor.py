7fe48f0f76601a9efbf7269038213cc43767e445
SBhuvaneswari <bhuvisiva24@gmail.com>
2022-11-21 10:42:22 +0530

    even and odd using for loop

---
+++ evenoddfor.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 10:40:08 2022
+
+@author: compro
+"""
+for number in range(1,11,1):
+    if number%2==0:
+        print(f"{number} is an even number")
+    else:
+        print(f"{number} is an odd number")
