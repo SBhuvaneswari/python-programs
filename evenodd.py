b4e1294e7435f9d5b7be17f9b8a0f72bfa367d52
SBhuvaneswari <bhuvisiva24@gmail.com>
2022-11-21 10:39:49 +0530

    even and odd using if statement

---
+++ evenodd.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 10:35:42 2022
+
+@author: compro
+"""
+
+number=int(input("Enter the number: "))
+if number%2==0:
+    print(f"{number} is an even number")
+else:
+    print(f"{number} is an odd number")
